a754ea0f0b43c43936ef8e7f349a301984563ae2
Philip <95943570+Srcyax@users.noreply.github.com>
2024-02-18 14:54:49 -0300

    Pizza Information

--- src/app/(pages)/Orders/orderCard.tsx
+++ src/app/(pages)/Orders/orderCard.tsx
@@ -3,48 +3,102 @@ import { Skeleton } from "@/components/ui/skeleton";
 import { useState } from "react";
 
 interface Order {
-    user: string;
-    address: string;
-    name: string;
-    pizzaImg: string;
+  user: string;
+  address: string;
+  name: string;
+  pizzaInfo: {
+    additionals: string;
+    observation: string;
+    delivery_method: string;
+    payment_method: string;
+  };
+  pizzaImg: string;
 }
 
 interface OrderCardProps {
-    user: string;
-    address: string;
-    name: string;
-    pizzaImg: string;
-    orders: Order[];
-    index: number;
-    onOrderComplete: (index: number, order: Order) => void;
+  user: string;
+  address: string;
+  name: string;
+  pizzaInfo: {
+    additionals: string;
+    observation: string;
+    delivery_method: string;
+    payment_method: string;
+  };
+  pizzaImg: string;
+  orders: Order[];
+  index: number;
+  onOrderComplete: (index: number, order: Order) => void;
 }
 
-export default function OrderCard({ user, address, name, pizzaImg, orders, index, onOrderComplete }: OrderCardProps){
-    const [isImageLoaded, setImageLoaded] = useState(false);
+export default function OrderCard({
+  user,
+  address,
+  name,
+  pizzaInfo,
+  pizzaImg,
+  orders,
+  index,
+  onOrderComplete,
+}: OrderCardProps) {
+  const [isImageLoaded, setImageLoaded] = useState(false);
 
-    const handleSetImageLoaded = () => {
-      setImageLoaded(true);
-    }
-    
-    const handleCompleteOrder = () => {
-        const order = orders[index];
-        onOrderComplete(index, order);
-    };
-    
-    return (
-        <div>
-            <div className="border-2 rounded-lg flex flex-col justify-center items-center">
-                {
-                    !isImageLoaded ? <Skeleton className="rounded-xl w-[200px] h-[150px] m-5"/> : null
-                }
-                <LazyLoadImage className={isImageLoaded ? "m-5 rounded-xl h-36" : ""} beforeLoad={handleSetImageLoaded} src={"images/" + pizzaImg} width={200} alt="" />
-                <h1>{name}</h1>
-                <div className="flex flex-col gap-4 m-5 w-52">
-                    <p className="text-center text-zinc-400">{"Cliente: " + user}</p>
-                    <p className="text-center text-zinc-400">{"Endereço: " + address}</p>
-                </div>
-                <button onClick={handleCompleteOrder} className="border-2 rounded-lg p-3 m-5 hover:text-orange-500 transition-all duration-200">Concluir</button>
-            </div>
+  const handleSetImageLoaded = () => {
+    setImageLoaded(true);
+  };
+
+  const handleCompleteOrder = () => {
+    const order = orders[index];
+    onOrderComplete(index, order);
+  };
+
+  return (
+    <div>
+      <div className="border-2 rounded-lg flex flex-col justify-center items-center">
+        {!isImageLoaded ? (
+          <Skeleton className="rounded-xl w-[200px] h-[150px] m-5" />
+        ) : null}
+        <LazyLoadImage
+          className={isImageLoaded ? "m-5 rounded-xl h-36" : ""}
+          beforeLoad={handleSetImageLoaded}
+          src={"images/" + pizzaImg}
+          width={200}
+          alt=""
+        />
+        <h1>{name}</h1>
+        <div className="flex flex-col gap-2 items-center m-5">
+          <h1>Informações da Pizza</h1>
+          <div className="flex flex-col items-center">
+            <p className="text-[13px]">
+              <strong>Adicionais:</strong>{" "}
+              {JSON.stringify(pizzaInfo.additionals)}
+            </p>
+            <p className="text-[13px]">
+              <strong>Método de entrega:</strong>{" "}
+              {JSON.stringify(pizzaInfo.delivery_method)}
+            </p>
+            <p className="text-[13px]">
+              <strong>Método de pagamento:</strong>{" "}
+              {JSON.stringify(pizzaInfo.payment_method)}
+            </p>
+            <p className="text-[13px]">
+              <strong>Observações:</strong>{" "}
+              {JSON.stringify(pizzaInfo.observation)}
+            </p>
+          </div>
         </div>
-    );
+
+        <div className="flex flex-col gap-4 m-5 w-52">
+          <p className="text-center text-zinc-400">{"Cliente: " + user}</p>
+          <p className="text-center text-zinc-400">{"Endereço: " + address}</p>
+        </div>
+        <button
+          onClick={handleCompleteOrder}
+          className="border-2 rounded-lg p-3 m-5 hover:text-orange-500 transition-all duration-200"
+        >
+          Concluir
+        </button>
+      </div>
+    </div>
+  );
 }
--- src/app/(pages)/Pizzas/PizzaCard.tsx
+++ src/app/(pages)/Pizzas/PizzaCard.tsx
@@ -1,6 +1,6 @@
 "use client";
 import axios from "axios";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { LazyLoadImage } from "react-lazy-load-image-component";
 import { Skeleton } from "@/components/ui/skeleton";
 import { toast } from "sonner";
@@ -25,8 +25,47 @@ import {
   SelectValue,
 } from "@/components/ui/select";
 
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+} from "@/components/ui/command";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+
+import { cn } from "@/lib/utils";
+
+import { Check, ChevronsUpDown } from "lucide-react";
+
 import { Checkbox } from "@/components/ui/checkbox";
+import { Button } from "@/components/ui/button";
 import { Textarea } from "@/components/ui/textarea";
+
+const payments = [
+  {
+    value: "dinheiro",
+    label: "Dinheiro",
+  },
+  {
+    value: "cartão",
+    label: "Cartão",
+  },
+];
+const delivery = [
+  {
+    value: "retirar",
+    label: "Retirar",
+  },
+  {
+    value: "entrega",
+    label: "Entrega",
+  },
+];
 
 interface CardInfo {
   user: string;
@@ -34,6 +73,13 @@ interface CardInfo {
   pizzaImg: string;
   pizzaName: string;
   pizzaDesc: string;
+}
+
+interface PizzaInformation {
+  additionals: string;
+  observation: string;
+  delivery_method: string;
+  payment_method: string;
 }
 
 export default function PizzaCardInfo({
@@ -48,6 +94,27 @@ export default function PizzaCardInfo({
   const handleSetImageLoaded = () => {
     setImageLoaded(true);
   };
+
+  const [openPayment, setOpenPayment] = useState(false);
+  const [openDelivery, setOpenDelivery] = useState(false);
+  const [valuePayment, setValuePayment] = useState("");
+  const [valueDelivery, setValueDelivery] = useState("");
+
+  const [additionals, setAdditionals] = useState("");
+  const [observation, setObservation] = useState("");
+  const [deliveryMethod, setDeliveryMethod] = useState("");
+  const [paymentMethod, setPaymentMethod] = useState("");
+  var pizzaInfo: PizzaInformation;
+
+  useEffect(() => {
+    pizzaInfo = {
+      additionals: additionals,
+      observation: observation,
+      delivery_method: deliveryMethod,
+      payment_method: paymentMethod,
+    };
+    console.log(pizzaInfo);
+  });
 
   return (
     <div className="border-2 rounded-lg flex flex-col justify-center items-center w-72">
@@ -84,7 +151,13 @@ export default function PizzaCardInfo({
               <h1 className="text-[14px]">Adicionais:</h1>
               <div className="flex flex-col gap-2">
                 <div className="flex items-center space-x-2">
-                  <Checkbox />
+                  <Checkbox
+                    onCheckedChange={(checked) => {
+                      return checked
+                        ? setAdditionals("Borda recheada")
+                        : setAdditionals("");
+                    }}
+                  />
                   <label className="text-sm text-center font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">
                     Borda recheada
                   </label>
@@ -95,6 +168,7 @@ export default function PizzaCardInfo({
                   Deixe sua observação
                 </label>
                 <Textarea
+                  onChange={(e) => setObservation(e.target.value)}
                   placeholder="Observações sobre o pedido."
                   id="message"
                 />
@@ -104,33 +178,111 @@ export default function PizzaCardInfo({
                   <label className="my-1 text-[14px]" htmlFor="">
                     Entrega ou retirada
                   </label>
-                  <Select>
-                    <SelectTrigger className="w-[180px]">
-                      <SelectValue placeholder="Retirar" />
-                    </SelectTrigger>
-                    <SelectContent>
-                      <SelectGroup>
-                        <SelectItem value="retirar">Retirar</SelectItem>
-                        <SelectItem value="entrega">Entrega</SelectItem>
-                      </SelectGroup>
-                    </SelectContent>
-                  </Select>
+                  <Popover open={openDelivery} onOpenChange={setOpenDelivery}>
+                    <PopoverTrigger asChild>
+                      <Button
+                        variant="outline"
+                        role="combobox"
+                        aria-expanded={openDelivery}
+                        className="w-[200px] justify-between"
+                      >
+                        {valueDelivery
+                          ? delivery.find(
+                              (delivery) => delivery.value === valueDelivery
+                            )?.label
+                          : "Selecione"}
+                        <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+                      </Button>
+                    </PopoverTrigger>
+                    <PopoverContent className="w-[200px] p-0">
+                      <Command>
+                        <CommandInput placeholder="Search framework..." />
+                        <CommandEmpty>No framework found.</CommandEmpty>
+                        <CommandGroup>
+                          {delivery.map((delivery) => (
+                            <CommandItem
+                              key={delivery.value}
+                              value={delivery.value}
+                              onSelect={(currentValue) => {
+                                setDeliveryMethod(currentValue);
+                                setValueDelivery(
+                                  currentValue === valueDelivery
+                                    ? ""
+                                    : currentValue
+                                );
+                                setOpenDelivery(false);
+                              }}
+                            >
+                              <Check
+                                className={cn(
+                                  "mr-2 h-4 w-4",
+                                  valueDelivery === delivery.value
+                                    ? "opacity-100"
+                                    : "opacity-0"
+                                )}
+                              />
+                              {delivery.label}
+                            </CommandItem>
+                          ))}
+                        </CommandGroup>
+                      </Command>
+                    </PopoverContent>
+                  </Popover>
                 </div>
                 <div className="flex flex-col">
                   <label className="my-1 text-[14px]" htmlFor="">
                     Método de pagamento
                   </label>
-                  <Select>
-                    <SelectTrigger className="w-[180px]">
-                      <SelectValue placeholder="Dinheiro" />
-                    </SelectTrigger>
-                    <SelectContent>
-                      <SelectGroup>
-                        <SelectItem value="dinheiro">Dinheiro</SelectItem>
-                        <SelectItem value="cartao">Cartão</SelectItem>
-                      </SelectGroup>
-                    </SelectContent>
-                  </Select>
+                  <Popover open={openPayment} onOpenChange={setOpenPayment}>
+                    <PopoverTrigger asChild>
+                      <Button
+                        variant="outline"
+                        role="combobox"
+                        aria-expanded={openPayment}
+                        className="w-[200px] justify-between"
+                      >
+                        {valuePayment
+                          ? payments.find(
+                              (payments) => payments.value === valuePayment
+                            )?.label
+                          : "Selecione"}
+                        <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+                      </Button>
+                    </PopoverTrigger>
+                    <PopoverContent className="w-[200px] p-0">
+                      <Command>
+                        <CommandInput placeholder="Search framework..." />
+                        <CommandEmpty>No framework found.</CommandEmpty>
+                        <CommandGroup>
+                          {payments.map((payments) => (
+                            <CommandItem
+                              key={payments.value}
+                              value={payments.value}
+                              onSelect={(currentValue) => {
+                                setPaymentMethod(currentValue);
+                                setValuePayment(
+                                  currentValue === valuePayment
+                                    ? ""
+                                    : currentValue
+                                );
+                                setOpenPayment(false);
+                              }}
+                            >
+                              <Check
+                                className={cn(
+                                  "mr-2 h-4 w-4",
+                                  valuePayment === payments.value
+                                    ? "opacity-100"
+                                    : "opacity-0"
+                                )}
+                              />
+                              {payments.label}
+                            </CommandItem>
+                          ))}
+                        </CommandGroup>
+                      </Command>
+                    </PopoverContent>
+                  </Popover>
                 </div>
               </div>
             </div>
@@ -139,12 +291,41 @@ export default function PizzaCardInfo({
             <AlertDialogCancel>Cancelar</AlertDialogCancel>
             <AlertDialogAction
               onClick={() => {
+                if (paymentMethod === "") {
+                  toast("Erro!", {
+                    description:
+                      "Por favor selecione corretamente o Método de pagamento.",
+                    action: {
+                      label: "Ok",
+                      onClick: () => console.log("Undo"),
+                    },
+                    duration: 3000,
+                  });
+
+                  return;
+                }
+
+                if (deliveryMethod === "") {
+                  toast("Erro!", {
+                    description:
+                      "Por favor selecione corretamente o Método de entrega.",
+                    action: {
+                      label: "Ok",
+                      onClick: () => console.log("Undo"),
+                    },
+                    duration: 3000,
+                  });
+
+                  return;
+                }
+
                 axios
                   .post("/api/orders", {
                     user: user,
                     address: address,
                     name: pizzaName,
                     desc: pizzaDesc,
+                    pizzaInfo: pizzaInfo,
                     pizzaImg: pizzaImg,
                   })
                   .then((res) => {
