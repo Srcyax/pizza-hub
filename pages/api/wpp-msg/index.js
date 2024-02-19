17dc8d93983dd6ac1e4b658f13abbc1fda1ffbe1
Philip <95943570+Srcyax@users.noreply.github.com>
2024-02-18 23:34:23 -0300

    Update index.js

--- pages/api/wpp-msg/index.js
+++ pages/api/wpp-msg/index.js
@@ -1,11 +1,18 @@
-import { Client } from "whatsapp-web.js";
+import { Client, LocalAuth } from "whatsapp-web.js";
 import qrcode from "qrcode-terminal";
 
 let client;
 
 async function initializeClient() {
 	if (!client) {
-		client = new Client({});
+		client = new Client({
+			puppeteer: {
+				headless: false,
+			},
+			authStrategy: new LocalAuth({
+				clientId: "PizzaHub",
+			}),
+		});
 
 		client.on("qr", (qr) => {
 			console.log("QR code");
@@ -21,18 +28,17 @@ async function initializeClient() {
 }
 
 export default async function handler(req, res) {
-	if (req.method === "POST") {
-		try {
-			await initializeClient();
-			const { number, message } = req.body;
-			const chat = await client.getChatById(`${number}@c.us`);
-			await chat.sendMessage(message);
-			res.status(200).json({ message: "Message sent successfully" });
-		} catch (error) {
-			console.error("Error sending message: ", error);
-			res.status(500).json({ message: "Error sending message" });
-		}
-	} else {
-		res.status(405).json({ message: "Method not allowed" });
+	switch (req.method) {
+		case "POST":
+			try {
+				await initializeClient();
+				const { number, message } = req.body;
+				const chat = await client.getChatById(`${number}@c.us`);
+				await chat.sendMessage(message);
+				return res.status(200).json({ message: "Message sent successfully" });
+			} catch (error) {
+				console.error("Error sending message: ", error);
+				return res.status(500).json({ message: "Error sending message" });
+			}
 	}
 }
