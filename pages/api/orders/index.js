dd742b87dfdb05da2758c63c174a8861faebfb12
Philip <95943570+Srcyax@users.noreply.github.com>
2024-02-19 00:23:30 -0300

    Update index.js

--- pages/api/orders/index.js
+++ pages/api/orders/index.js
@@ -1,19 +1,18 @@
 export var orders = [];
 
 export default function handler(req, res) {
-  const { method } = req;
+	const { method } = req;
 
-  switch (method) {
-    case "POST":
-      orders.push(req.body);
-      console.log(orders);
-      res.status(200).json({
-        message: "Sucess",
-        orders,
-      });
-      break;
-    case "GET":
-      res.status(200).json({ orders });
-      break;
-  }
+	switch (method) {
+		case "POST":
+			orders.push(req.body);
+			res.status(200).json({
+				message: "Sucess",
+				orders,
+			});
+			break;
+		case "GET":
+			res.status(200).json({ orders });
+			break;
+	}
 }
