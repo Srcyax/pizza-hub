50146994f4dbd81c0909549ce73921d8b3c2af05
Philip <95943570+Srcyax@users.noreply.github.com>
2024-02-19 04:02:04 -0300

    New menu/Avatar

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -1,7 +1,31 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+
+import {
+	Sheet,
+	SheetClose,
+	SheetContent,
+	SheetDescription,
+	SheetFooter,
+	SheetHeader,
+	SheetTitle,
+	SheetTrigger,
+} from "@/components/ui/sheet";
+
 import React from "react";
 import Link from "next/link";
+import { useState, useEffect } from "react";
+import { AlignJustify } from "lucide-react";
 
 export default function Header() {
+	const [user, getUser] = useState<string>("");
+
+	useEffect(() => {
+		getUser(localStorage.getItem("user") ?? "");
+	});
+
 	return (
 		<header className="flex flex-row justify-between m-5">
 			<Link className="group" href="/">
@@ -13,12 +37,41 @@ export default function Header() {
 				</h1>
 			</Link>
 			<div className="flex flex-row gap-5 items-center">
-				<Link href="/Pizzas">
-					<h1 className="hover:text-orange-400 transition-all">Pizzas</h1>
-				</Link>
-				<Link href="/Orders">
-					<h1 className="hover:text-orange-400 transition-all">Pedidos</h1>
-				</Link>
+				<Sheet>
+					<SheetTrigger asChild>
+						<Button variant="outline">
+							<AlignJustify />
+						</Button>
+					</SheetTrigger>
+					<SheetContent>
+						<div className="flex flex-row items-center gap-2">
+							<Avatar>
+								<AvatarFallback className="p-4">
+									{user.charAt(0).toUpperCase()}
+								</AvatarFallback>
+							</Avatar>
+							<label htmlFor="">{user}</label>
+						</div>
+
+						<div className="flex flex-col items-center m-14 gap-6">
+							<Link href="/">
+								<h1 className="text-[20px] hover:text-orange-400 transition-all">
+									Home
+								</h1>
+							</Link>
+							<Link href="/Pizzas">
+								<h1 className="text-[20px] hover:text-orange-400 transition-all">
+									Pizzas
+								</h1>
+							</Link>
+							<Link href="/Orders">
+								<h1 className="text-[20px] hover:text-orange-400 transition-all">
+									Pedidos
+								</h1>
+							</Link>
+						</div>
+					</SheetContent>
+				</Sheet>
 			</div>
 		</header>
 	);
