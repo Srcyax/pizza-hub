fdafa83b4d752e2c5b8ecad2f5972137378d7a54
Philip <95943570+Srcyax@users.noreply.github.com>
2024-02-19 00:17:46 -0300

    Fix?

--- pages/api/wpp-msg/index.js
+++ pages/api/wpp-msg/index.js
@@ -1,34 +1,43 @@
 import { Client, LocalAuth } from "whatsapp-web.js";
 import qrcode from "qrcode-terminal";
 
-const client = new Client({
-	puppeteer: {
-		headless: false,
-	},
-	authStrategy: new LocalAuth({
-		clientId: "PizzaHub",
-	}),
-});
+let client;
 
-client.on("qr", (qr) => {
-	console.log("QR code");
-	qrcode.generate(qr, { small: true });
-});
+async function initialize() {
+	if (!client) {
+		client = new Client({
+			puppeteer: {
+				headless: false,
+			},
+			authStrategy: new LocalAuth({
+				clientId: "PizzaHub",
+			}),
+		});
 
-client.on("ready", () => {
-	console.log("Client is ready");
-});
+		client.on("qr", (qr) => {
+			console.log("QR code");
+			qrcode.generate(qr, { small: true });
+		});
+
+		client.on("ready", () => {
+			console.log("Client is ready");
+		});
+
+		await client.initialize();
+	}
+}
 
 export default async function handler(req, res) {
 	switch (req.method) {
 		case "POST":
 			try {
-				client.initialize().then(async () => {
-					const { number, message } = req.body;
-					const chat = await client.getChatById(`${number}@c.us`);
-					await chat.sendMessage(message);
+				await initialize();
+				const { number, message } = req.body;
+				const chat = await client.getChatById(`${number}@c.us`);
+				await chat.sendMessage(message).then(() => {
+					return res.status(200).json({ message: "Message sent successfully" });
 				});
-				return res.status(200).json({ message: "Message sent successfully" });
+				return;
 			} catch (error) {
 				console.error("Error sending message: ", error);
 				return res.status(500).json({ message: "Error sending message" });
